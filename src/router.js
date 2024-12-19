26f593a389ea3819cd464889015492cbfdad82c5
jabberwocky238 <jabberwocky238@gmail.com>
2024-12-19 21:22:01 +0800

    feat: no more views, the easier the better

--- src/router.js
+++ src/router.js
@@ -1,13 +1,13 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Dashboard from "@/views/Dashboard.vue";
+import CollaborationGraph from "@/components/CollaborationGraph.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
-      name: "home",
-      component: Dashboard,
+      name: "CollaborationGraph",
+      component: CollaborationGraph,
     },
     // {
     //   path: '/about',
